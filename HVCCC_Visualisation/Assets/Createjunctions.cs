85bcc9802bc1a63a92e9224a32ba9ee2b30ecfc5
James Valencia <c3235715@uon.edu.au>
2019-05-12 16:48:32 +1000

    Adds code to get the excel data into unity
    I saved the excel as a csv and imported into the folder called Resources, i added some code to read the rows and split them into each part separated by a comma. I also added a new class called AllJucntions that can hold the individual data for each junction and added each of those junctions to a list inside the createjunctions class. Use as much as you need.

--- HVCCC_Visualisation/Assets/Createjunctions.cs
+++ HVCCC_Visualisation/Assets/Createjunctions.cs
@@ -17,6 +17,8 @@ public class Createjunctions : MonoBehaviour
     public double bottomright_lat = -33.854337;
     public double bottomright_lon = 152.634515;
 
+    List<AllJunctions> junctions = new List<AllJunctions>();
+
 
     //Topleft and Bottomright will need to represent the correct lattitudes and longitudes later.
     // Start is called before the first frame update
@@ -33,7 +35,37 @@ public class Createjunctions : MonoBehaviour
         double topleft_bottomright_length_z = bottomright.transform.position.z - topleft.transform.position.z;
         double jn_pos_z = topleft.transform.position.z + topleft_bottomright_length_z * (jn_lon_distance / lon_distance);
 
+        //EXCEL PART, THE CSV IS IN THE RESOURCES FOLDER
+        TextAsset junctionData = Resources.Load<TextAsset>("Junctions_Coordinates");
+        string[] data = junctionData.text.Split(new char[] { '\n' });
+        //Debug.Log(data.Length);
+        for(int i = 1; i < data.Length; i++)
+        {
+            string[] row = data[i].Split(new char[] { ',' });
+            AllJunctions j = new AllJunctions();
 
+            //gets the data for each row and adds to class AllJunctions
+            j.id = row[0];
+
+            j.signalName = row[1];
+
+            string x = row[2];
+            x = x.Substring(1);
+            double xCoord;
+            double.TryParse(x, out xCoord);
+            j.xCoordinate = xCoord;
+
+            string z = row[3];
+            z = z.Replace("\"", "");
+            double zCoord;
+            double.TryParse(z, out zCoord);
+            j.zCoordinate = zCoord;
+
+            //DOES ARE ALL THE BITS OF DATA IN EXCEL, THE IMPORTANT ONES AT LEAST
+            Debug.Log(row[0] + ", " + row[1] + ", " + xCoord + ", " + zCoord);
+
+            junctions.Add(j);
+        }
 
 
         //topleft.position.x
