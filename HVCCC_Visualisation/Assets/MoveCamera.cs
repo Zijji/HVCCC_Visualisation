b70f4d72503010a826621414be726d69dcd50563
Edward <eingle101@gmail.com>
2019-05-15 15:30:49 +1000

    Implemented camera based on Unity camera package. WASD to move map.

---
+++ HVCCC_Visualisation/Assets/MoveCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    private Transform thisTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = -Input.GetAxis("Horizontal");
+        float moveZ = -Input.GetAxis("Vertical");
+
+        thisTransform.position = new Vector3(thisTransform.position.x + moveX * moveSpeed, 0.0f, thisTransform.position.z + moveZ * moveSpeed);
+    }
+}
