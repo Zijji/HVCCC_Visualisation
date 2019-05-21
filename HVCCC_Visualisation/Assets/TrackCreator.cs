f0e4424d4b9e0c90ed8c329520b22c1a52eaa49f
Edward <eingle101@gmail.com>
2019-05-21 23:47:43 +1000

    Code for creating sections, will need the tracks to complete fully.

---
+++ HVCCC_Visualisation/Assets/TrackCreator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCreator : MonoBehaviour
+{
+    public GameObject TrackStart;
+    public GameObject TrackEnd;
+    public GameObject TrackObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 TrackPosition = TrackStart.transform.position;
+
+        /*
+        boolean quit = false;
+        while(!quit)
+        {
+            
+        }
+        */
+        //for(int i = 0; i < 50; i++)
+        while(Vector3.Distance(TrackStart.transform.position, TrackPosition) < Vector3.Distance(TrackEnd.transform.position, TrackStart.transform.position))
+        {
+            Instantiate(TrackObject, TrackPosition, TrackStart.transform.rotation);
+            TrackPosition += (TrackEnd.transform.position - TrackStart.transform.position).normalized;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
