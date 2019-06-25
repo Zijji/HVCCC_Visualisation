37d20233c3c1a9df035a51d9cd9a68945e36ccfe
Nathan Ebba <ebbanat@gmail.com>
2019-06-25 17:59:00 +1000

    Changed path to generic data structures. In the process of sb.append to sb.appendformat

--- HVCCC_Visualisation/Assets/Trains/SpawnTrains.cs
+++ HVCCC_Visualisation/Assets/Trains/SpawnTrains.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Schemas;
 using System;
+using System.Text;
 
 public class SpawnTrains : MonoBehaviour
 {
@@ -22,7 +23,8 @@ public class SpawnTrains : MonoBehaviour
         string[] timetable; // references if the direciton is up or down the valley
         string[] days; // Days travelled 1 day = 1440 minutes
         string[] minutes; // Times at the junctions minutes after midnight
-        
+        StringBuilder sb = new StringBuilder();
+
         // Retreive all the paths from XML
         for (int i = 0; i < xml_helper.getPaths().paths.Length; i++)
         {
@@ -39,8 +41,35 @@ public class SpawnTrains : MonoBehaviour
 
             days = current.daysOfWeek.Split(' ');
             minutes = current.timeAtJunction.Split(' ');
-            
+
+            foreach (string temp in minutes)
+            {
+                print(temp);
+            }
+
             // Probably using StringBuilder to build before adding to the data structure called paths.
+
+            foreach (string d in days)
+            {
+                sb.Clear(); // Clear the string builder for a new path
+
+                foreach (string m in minutes)
+                {
+                    if (m.Equals("-1000"))
+                    {
+                        print("Case being ignored because j = 10000");
+                        break; // Just ignore this junction it is not visited path (or the train) for this instance
+                    }
+                    else
+                    {
+                        /* Add a new destination for the path. */
+                        sb.Append() // Currently appending nothing but should look into StringBuilder.AppendFormat()
+//        https://docs.microsoft.com/en-us/dotnet/api/system.text.stringbuilder?view=netframework-4.8#methods
+                    }
+                }
+                
+                // Insert into data structure called paths
+            }
 
             // Checks the number of days and creates a PathCreator object for each day.
 //            for (int k = 0; k < daysTravelsOnPath.Length; k++)
