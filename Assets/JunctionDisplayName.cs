c188fe643960cc95ed488172d099693ef3b79ac9
clickk <nicholaskhendry@gmail.com>
2019-08-19 13:21:40 +1000

    Junctions added to Mapbox, Junction labels added

---
+++ Assets/JunctionDisplayName.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunctionDisplayName : MonoBehaviour
+{
+    [SerializeField]
+    TextMesh _textMesh; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        _textMesh.text = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
