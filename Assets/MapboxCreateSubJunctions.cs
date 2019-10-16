bd385b309d8b5fa74c3614f400ae449af556683f
Edward <eingle101@gmail.com>
2019-10-17 07:59:47 +1100

    Removed unused SubjunctionFromGeojson class Added direct input from Geojson file for subjunctions Disabled loading subjunction game objects

--- Assets/MapboxCreateSubJunctions.cs
+++ Assets/MapboxCreateSubJunctions.cs
@@ -41,23 +41,72 @@ public class MapboxCreateSubJunctions : MonoBehaviour
     {
 
         //Currently looks at the geojson file directly to get the track coordinates
-        //Debug.Log("Features:"+ SubjunctionFromGeojson.Create().features);
-        /*
+        //May need to get from xml file.
+
         //Returns json from the file.
-        string geJsonPath = "hunter_valley_tracks.geojson";
+        string getJsonPath = "hunter_valley_tracks.geojson";
         string getJsonString = "";
-        StreamReader sr = new StreamReader(geJsonPath); 
+        StreamReader sr = new StreamReader(getJsonPath); 
         getJsonString += sr.ReadToEnd();
         sr.Close();
+/*
+"features": [
+{ "type": "Feature", "properties": { "osm_id": "2654260", "code": 6102, "fclass": "light_rail", "name": "Inner West Light Rail" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 151.1964198, -33.868837 ], [ 151.1962464, -33.8687557 ] ] ] } },
 
-        var GJ = JSON.Parse(getJsonString);
-        string example = GJ["features"][0]["type"].Value;
+ */
+ /*
+        string example = GJ["features"][0]["properties"]["name"].Value;
         Debug.Log("value:"+example);
+        string geometry = GJ["features"][0]["geometry"]["coordinates"][0][0][0].Value;
+        Debug.Log("geometry:"+geometry);
+        Debug.Log("isNull?:"+ geometry==null);
+        var arrayCount = GJ["features"][0]["geometry"]["coordinates"][0][0][2].Value;
+        Debug.Log("end of array:"+arrayCount);
+        Debug.Log("isEmpty?:"+ arrayCount.Equals(""));
+  */
+        
+        //var thisCoord = GJ["features"][0]["geometry"]["coordinates"][0][0][0].Value;
+        // 1: spawn all subjunctions   2:link subjunctions  3: get closest junctions to subjunctions  4: 
+
+        
+
+
+        var getGeoJson = JSON.Parse(getJsonString);
+        /*
+        Debug.Log("array length:" + );
+        Debug.Log("end of array0:" + getGeoJson["features"][0]["geometry"]["coordinates"][0][0][0].Value.Equals(""));
+        Debug.Log("end of array1:" + getGeoJson["features"][0]["geometry"]["coordinates"][0][0][1].Value.Equals(""));
+        Debug.Log("end of array2:" + getGeoJson["features"][0]["geometry"]["coordinates"][0][0][2].Value.Equals(""));
          */
+        Debug.Log(getGeoJson["features"].AsArray.Count);
+        Debug.Log(getGeoJson["features"][1]["geometry"]["coordinates"][0].AsArray.Count);
+        
+        List<string> allSubJunctions = new List<string>();
+
         
         
+        for(int i1 = 0; i1 < getGeoJson["features"].AsArray.Count; i1++)
+        {
+            for(int i2 = 0; i2 < getGeoJson["features"][i1]["geometry"]["coordinates"][0].AsArray.Count; i2++)
+            {
+                allSubJunctions.Add(getGeoJson["features"][i1]["geometry"]["coordinates"][0][i2][1].Value + ", " + getGeoJson["features"][i1]["geometry"]["coordinates"][0][i2][0].Value);
+            }
+        }
+        //Removes duplicate subjunction coords
+        var distinctSubjunctions = new HashSet<string>(allSubJunctions);
+        
+        allSubJunctions = new List<string>(distinctSubjunctions);
+        /*
+        for(i = 0; !getGeoJson["features"][i].Equals(""); i++)
+        {
+            for(i2 = 0; !getGeoJson["features"][i]["geometry"]["coordinates"][0][i2].Equals(""); i2++)
+            {
+                allSubJunctions.Add(getGeoJson["features"][i]["geometry"]["coordinates"][0][i2][0].Value + "," + getGeoJson["features"][i]["geometry"]["coordinates"][0][i2][1].Value);
+            }
+        }
+        */
 
-
+        /*
         List<List<List<float>>> allTracks = xml_helper.getAllTrackCoords();
         List<string> allSubJunctions = new List<string>();
         
@@ -68,6 +117,8 @@ public class MapboxCreateSubJunctions : MonoBehaviour
                 allSubJunctions.Add(Convert.ToString(subTracks[1]) + "," + Convert.ToString(subTracks[0]));
             }
         }
+         */
+
 
         List<string> subJunctionNames = new List<string>();
 
@@ -88,6 +139,9 @@ public class MapboxCreateSubJunctions : MonoBehaviour
 
         _locations = new Vector2d[_locationStrings.Length];
         _spawnedObjects = new List<GameObject>();
+        /*Disabled placement code for locations for testing
+        
+        
         for (int i = 0; i < _locationStrings.Length; i++)
         {
             var locationString = _locationStrings[i];
@@ -102,7 +156,7 @@ public class MapboxCreateSubJunctions : MonoBehaviour
             }
             _spawnedObjects.Add(instance);
         }
-
+        */
         zoom = _map.AbsoluteZoom;
     }
 
