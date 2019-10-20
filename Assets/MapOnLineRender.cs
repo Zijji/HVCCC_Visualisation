10ff95c5e8e21b73da077557c0f0163f5bdb2201
chrischapman1 <48075652+chrischapman1@users.noreply.github.com>
2019-10-21 10:50:18 +1100

    Please revert

---
+++ Assets/MapOnLineRender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapOnLineRender : MonoBehaviour
+{
+    public Transform sampleTrain;
+    
+    private LineRenderer line;
+    private Junction junction1;
+    private Junction junction2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sampleTrain.position = new Vector3();
+        
+        line.SetPosition(0, sampleTrain.position);
+
+        line.enabled = false;
+
+    }
+
+    private void placeOnTrack(Vector3 position)
+    {
+        return;
+    }
+}
