c157c45c2a4b4a6f348749089a255bae7c739e63
clickk <nicholaskhendry@gmail.com>
2019-11-04 12:27:50 +1100

    Merged pathing scripts

--- Assets/MapboxCreateJunctions.cs
+++ Assets/MapboxCreateJunctions.cs
@@ -6,9 +6,9 @@ using Mapbox.Unity.Map;
 using Mapbox.Unity.MeshGeneration.Factories;
 using Mapbox.Unity.Utilities;
 
-//Creates the junctions using the mapbox scaling
 public class MapboxCreateJunctions : MonoBehaviour
 {
+    public XMLHelper xml_helper = new XMLHelper();
     List<AllJunctions> junctions = new List<AllJunctions>();
 
     [SerializeField]
@@ -25,24 +25,23 @@ public class MapboxCreateJunctions : MonoBehaviour
     [SerializeField] 
     GameObject _markerPrefab;
     
-    //Holds all junctions in one gameobject
+    
     public GameObject _junctionParent;
     
     List<GameObject> _spawnedObjects;
 
     public float zoom;
     
+    MapboxCreateSections thisMBCS;
 
     void Start()
     {
-	
+        thisMBCS = GetComponent<MapboxCreateSections>();
         //EXCEL PART, THE CSV IS IN THE RESOURCES FOLDER
         TextAsset junctionData = Resources.Load<TextAsset>("Junctions_Coordinates");
         string[] data = junctionData.text.Split(new char[] { '\n' });
         List<string> junctionLocations = new List<string>();
         List<string> junctionNames = new List<string>();
-        
-        //Iterates through the data of the junctions
         for (int i = 1; i < data.Length - 1; i++)
         {
             string[] row = data[i].Split(new char[] { ',' });
@@ -51,43 +50,62 @@ public class MapboxCreateJunctions : MonoBehaviour
             //gets the data for each row and adds to class AllJunctions
             j.id = row[0];
 
-            //Gets name
             j.signalName = row[1];
-            
-            //Latitude
+            //Debug.Log("he");
             string x = row[2];
+            //            Debug.Log(x);
             x = x.Substring(1);
             double xCoord;
             double.TryParse(x, out xCoord);
             j.xCoordinate = xCoord;
 
-            //Longitude
             string z = row[3];
             z = z.Replace("\"", "");
             double zCoord;
             double.TryParse(z, out zCoord);
             j.zCoordinate = zCoord;
 
-            //Adds to a list
             junctions.Add(j);
             junctionLocations.Add(x + "," + z);
             junctionNames.Add(row[0]);
         }
+        /*
+        AllJunctions j1 = new AllJunctions();
+        j1.id = "id1";
+        j1.signalName = "signal1";
+        j1.xCoordinate = -33.9324425;
+        j1.zCoordinate = 151.1910184;
+        junctions.Add(j1);
+         *
+        junctionLocations.Add("-33.9324425,151.1910184");
+        junctionLocations.Add("-33.9325201,151.1914601");
+        junctionLocations.Add("-33.9326104,151.19183");
+        junctionLocations.Add("-33.9327024,151.1920802");
+        junctionLocations.Add("-33.9328467,151.192379");
+        junctionNames.Add("junction1");
+        junctionNames.Add("junction2");
+        junctionNames.Add("junction3");
+        junctionNames.Add("junction4");
+        junctionNames.Add("junction5");
+         */
+
+
 
         //Inserts the junctions to the start of the location strings array. 
         string[] junctionLocationsArr = junctionLocations.ToArray();
         var allLocations = new string[_locationStrings.Length + junctionLocationsArr.Length];
 
-        //Begins mapbox conversion
         junctionLocationsArr.CopyTo(allLocations, 0);
         _locationStrings.CopyTo(allLocations, junctionLocationsArr.Length);
+        /*
+        _locationStrings.CopyTo(allLocations, 0);
+        junctionLocationsArr.CopyTo(allLocations, _locationStrings.Length);
+        */
         _locationStrings = allLocations;
         
-        //Uses gameobjects
+
         _locations = new Vector2d[_locationStrings.Length];
         _spawnedObjects = new List<GameObject>();
-
-        //Iterates through the locations and makes the new mapbox friendly gameobjects
         for (int i = 0; i < _locationStrings.Length; i++)
         {
             var locationString = _locationStrings[i];
@@ -95,25 +113,24 @@ public class MapboxCreateJunctions : MonoBehaviour
             var instance = Instantiate(_markerPrefab);
             instance.transform.parent = _junctionParent.transform;
             instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
-
-            //Checks position based on scale (zoom)
             instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
             if (i < junctionNames.Count)
             {
                 instance.name = junctionNames[i];
             }
+
             _spawnedObjects.Add(instance);
         }
+    
         zoom = _map.AbsoluteZoom;
+        thisMBCS.AssignSections();
     }
 
-    //Called on every frame, checks for zoom
     void Update()
     {
         if (zoom != _map.AbsoluteZoom)
         {
             int count = _spawnedObjects.Count;
-            //refreshes the junction positions based on the map zoom
             for (int i = 0; i < count; i++)
             {
                 var spawnedObject = _spawnedObjects[i];
@@ -126,25 +143,10 @@ public class MapboxCreateJunctions : MonoBehaviour
         }
     }
     
-    //When the user interacts with the program 
     void OnGUI()
     {
-        //On scroll, mouse drag, WASD
         if (Event.current.type == EventType.ScrollWheel || Event.current.type == EventType.MouseDrag || 
-            Event.current.type == EventType.MouseUp || Event.current.type == EventType.MouseDown || Event.current.Equals(Event.KeyboardEvent("w"))
-            || Event.current.Equals(Event.KeyboardEvent("a")) || Event.current.Equals(Event.KeyboardEvent("s")) || Event.current.Equals(Event.KeyboardEvent("d")))
-        {
-            int count = _spawnedObjects.Count;
-            //Updates coords
-            for (int i = 0; i < count; i++)
-            {
-                var spawnedObject = _spawnedObjects[i];
-                var location = _locations[i];
-                spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-                spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-            }
-        }
-        else
+            Event.current.type == EventType.MouseUp || Event.current.type == EventType.MouseDown)
         {
             int count = _spawnedObjects.Count;
             for (int i = 0; i < count; i++)
@@ -155,6 +157,19 @@ public class MapboxCreateJunctions : MonoBehaviour
                 spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
             }
         }
+        
+//        Debug.Log(Event.current.type);
+    }
+
+    public void setLocationStrings(string[] newStrings)
+    {
+        _locationStrings = newStrings;
+        _locations = new Vector2d[_locationStrings.Length];
+        
+    }
+    public string[] getLocationStrings()
+    {
+        return _locationStrings;
     }
 
 }
